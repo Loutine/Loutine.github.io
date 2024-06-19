569fc1b778dc71bc27359921e87b06e7227fe071
Loutine <uhuru-loutine@outlook.com>
2024-06-19 21:50:33 +0800

    add all_tags.html to show all site tags

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.default = 
+      with import nixpkgs { system = "x86_64-linux"; };
+      pkgs.mkShellNoCC {
+      packages = with nixpkgs; [
+      		zola
+		hugo
+	];
+      };
+
+  };
+}
